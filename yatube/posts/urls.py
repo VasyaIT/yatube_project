e0ae21a5360199aae596beedc7a55debc01f7dbd
Vasya <oiuy78@yandex.ru>
2023-03-11 16:41:01 +0300

    Add css and models

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('group/<slug:slug>/', views.group_posts, name='group'),
+    path('about/', views.about, name='about'),
+    path('group/<str:slug>/', views.group_posts, name='group'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+from .models import Post
+
 
 def index(request):
-    return render(request, 'posts/index.html')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def about(request):
+    return render(request, 'posts/about.html')
+
+
+def group_posts(request, slug):
+    return render(request, 'posts/group_posts.html')
